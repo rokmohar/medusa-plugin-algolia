4017e894da5b68e871d5b7a0f5d6767a89854750
Rok Mohar <rok.mohar@gmail.com>
2025-01-31 18:09:26 +0100

    Export service types;

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { AlgoliaService } from './modules/algolia/services'
+export { AlgoliaPluginOptions, SearchOptions } from './modules/algolia/types'
--- src/modules/algolia/types/index.ts
+++ src/modules/algolia/types/index.ts
@@ -1,15 +1,1 @@
-import { SearchTypes } from '@medusajs/types'
-
-export type SearchOptions = {
-  filter: string
-  paginationOptions: Record<string, unknown>
-  additionalOptions: Record<string, unknown>
-}
-
-export interface AlgoliaPluginOptions {
-  applicationId: string
-  adminApiKey: string
-  settings?: {
-    [key: string]: SearchTypes.IndexSettings
-  }
-}
+export * from './algolia'
